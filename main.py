db438fab5b728a5ab499b8e4337d2025c6c62be0
BigBosko <bskubic07@gmail.com>
2025-11-03 21:10:20 +0100

    Test button + enviorment file
    Tried some button functionality by following custom kinter tutorial: https://www.youtube.com/watch?v=WE1IuPOICxE&list=PLfZw_tZWahjxJl81b1S-vYQwHs_9ZT77f&index=2

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print("hello world")
+import tkinter
+import customtkinter
+
+#theme and color
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+root = customtkinter.CTk()
+
+root.title('UserAuth')
+root.geometry('600x350')
+
+click_counter=0
+
+def first_button_action():
+    global click_counter
+    click_counter+=1
+    first_label.configure(text=f"Button clicked {click_counter} times")
+    pass
+
+
+first_button = customtkinter.CTkButton(
+    root,
+    text="Helo World",
+    command=first_button_action,
+    height=100,
+    width=200,
+    font=("Helvetica", 24),
+    text_color="white",
+    fg_color="blue",
+    bg_color="white",
+    hover_color="purple",
+    corner_radius=50,
+    border_width=2,
+    border_color="yellow",
+    )
+
+first_button.pack(pady=80)
+
+first_label = customtkinter.CTkLabel(root, text="Button clicked 0 times")
+first_label.pack(pady=20)
+
+
+
+root.mainloop()
