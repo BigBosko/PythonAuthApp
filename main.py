3647f3144c0bb5d187b4dd2b8cc4950dfa391f70
Boštjan Skubic <bskubic07@gmail.com>
2025-11-04 11:20:53 +0100

    Username and pass entry box
    Added the username and pass entry box. No functionality yet

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ root.title('UserAuth')
 root.geometry('600x350')
 
 click_counter=0
+username=None
+password=None
 
 def first_button_action():
     global click_counter
     click_counter+=1
     first_label.configure(text=f"Button clicked {click_counter} times")
     pass
-
 
 first_button = customtkinter.CTkButton(
     root,
@@ -35,7 +36,19 @@ first_button = customtkinter.CTkButton(
     border_color="yellow",
     )
 
-first_button.pack(pady=80)
+username_entry = customtkinter.CTkEntry(
+    root, 
+    placeholder_text="username"
+    )
+password_entry = customtkinter.CTkEntry(
+    root,
+    placeholder_text="password"
+)
+
+username_entry.pack(pady=20)
+password_entry.pack(pady=10)
+
+first_button.pack(pady=20)
 
 first_label = customtkinter.CTkLabel(root, text="Button clicked 0 times")
 first_label.pack(pady=20)
